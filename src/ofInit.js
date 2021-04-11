73ae642b4dd3dd140a75d110e273b2e68a087fa1
of <you@example.com>
2021-04-11 21:19:39 +0800

    完成c_cpp_properties.json自动配置_未清理注释

---
+++ src/ofInit.js
@@ -0,0 +1,237 @@
+// const { exit } = require('node:process');
+const vscode = require('vscode');
+
+// for(let key in vscode){ 
+// 	console.log('key:', key);
+// }
+
+function get_includePath(log_wmake){
+    const fs=require('fs');
+    var data = fs.readFileSync(log_wmake,'utf8');
+    var m = data.match(/\-I(\S*)/g);
+    return m;
+}
+
+// function filter_src(src_path){
+//     var OFpath = vscode.workspace.getConfiguration('ofextension').get('OFpath');
+//     var OFsrc = vscode.workspace.getConfiguration('ofextension').get('OFsrc');
+//     // 有的src使用OF的，有的是自定义的
+//     if(`${OFpath}/src`!==OFsrc  // 自定义了OFsrc，并且Make/files中还自定义了
+//         &&){
+//     }
+// }
+
+function sort_uniq_pp(arr){
+    var fs=require('fs');
+    // sort and uniq
+    arr.sort();
+    // var hash=[arr[0]];
+    var hash=[];
+    console.log('提取到的头文件路径：')
+    // 要检查目录是否存在
+    for (let i = 0; i < arr.length; i++) {
+        // 添加第1个元素
+        arr[i]=arr[i].replace('-I','');
+        if (hash.length===0 && arr[i]!=='.'){
+            try {
+                fs.accessSync(arr[i],fs.constants.F_OK);
+                console.log(`exist: ${arr[i]}`);
+                hash.push(arr[i]);
+            } catch (err) {
+                console.log(`not exist: ${arr[i]}`);
+            }
+
+            // fs.access(arr[i],(err)=>{
+            //     if(!err){
+            //         console.log('exist');
+            //         hash.push(arr[i]);
+            //     }else{
+            //         console.log('not exist');
+            //     }
+            // });
+        } 
+        // 添加后续元素，保证与已有的最后一个元素不相同
+        if(hash.length>=1 
+            && arr[i]!==hash[hash.length-1] 
+            && arr[i]!=='-I.') {
+                try {
+                    fs.accessSync(arr[i],fs.constants.F_OK);
+                    console.log(`exist: ${arr[i]}`);
+                    hash.push(arr[i]);
+                } catch (err) {
+                    console.log(`not exist: ${arr[i]}`);
+                }
+            // fs.access(arr[i],(err)=>{
+            //     if(!err){
+            //         hash.push(arr[i]);
+            //     }
+            // })
+        }
+    }
+
+// TODO:  2021-4-11，等待编译结束才能解析includePath
+
+    // post process: 修改src与否；lnInclude后面加上/*
+    // for (let i = 0; i < hash.length; i++) {
+    //     hash[i]=hash[i].replace('-I','');
+    //     if (hash[i].match(/lnInclude$/)){
+    //         hash[i]=hash[i].replace(/$/,'/*')
+    //     }
+    // }
+    return hash;
+}
+
+
+function gen_cpp_conf(err,m_uniq,wkspacefd){
+    const fs=require('fs');
+    console.log(m_uniq);
+    if(err){
+        console.log('---配置文件.vscode/c_cpp_properties.json 不存在，重新创建')
+        m_uniq=sort_uniq_pp(m_uniq);
+        m_uniq.push('./**');
+        var c_cpp={
+                "configurations": [
+                    {
+                        "name": "Linux",
+                        "includePath": m_uniq,
+                        "defines": [],
+                        "compilerPath": "/usr/bin/gcc",
+                        "cStandard": "gnu11",
+                        "cppStandard": "gnu++14",
+                        "intelliSenseMode": "gcc-x64"
+                    }
+                ],
+                "version": 4 
+            }
+        var c_cpp_json = JSON.stringify(c_cpp,null,2);
+        console.log('---更新c_cpp_peroperties.json内容:\n',c_cpp_json);
+        fs.writeFileSync(`${wkspacefd}/.vscode/c_cpp_properties.json`,c_cpp_json);
+    }else{
+        console.log('---配置文件已存在，更新includePath')
+        var c_cpp_raw=fs.readFileSync(`${wkspacefd}/.vscode/c_cpp_properties.json`);
+        var c_cpp=JSON.parse(c_cpp_raw);
+        // 合并头文件列表，还需要去重
+        c_cpp.configurations[0].includePath.push(...m_uniq);
+        c_cpp.configurations[0].includePath = sort_uniq_pp(c_cpp.configurations[0].includePath);
+        c_cpp.configurations[0].includePath.push('./**');
+        var c_cpp_json2=JSON.stringify(c_cpp,null,2);
+        console.log('---更新c_cpp_properties.json内容:\n'+c_cpp_json2);
+        fs.writeFileSync(`${wkspacefd}/.vscode/c_cpp_properties.json`,c_cpp_json2);
+    }
+}
+
+// module.exports = function(context) {
+//     context.subscriptions.push(vscode.commands.registerCommand('test-taskprovider.ofInit', ()=>{
+//         var fs=require('fs');
+// 		var wkspacefd=vscode.workspace.workspaceFolders[0].uri.fsPath;
+// 		var logfile=`${wkspacefd}/log.wmake`;
+//         fs.access(`${wkspacefd}/.vscode/c_cpp_properties.json`,fs.constants.F_OK,(err)=>{
+//             var m = get_includePath(logfile);
+//             var m_uniq=sort_uniq_pp(m);
+//             gen_cpp_conf(err,m_uniq,wkspacefd);
+//         });
+//     }));
+// }
+
+module.exports = function(context) {
+    // 注册HelloWord命令, 
+    // 所有注册类的API执行后都需要将返回结果放到`context.subscriptions`中去
+    context.subscriptions.push(vscode.commands.registerCommand('ofextension.ofInit', async () => {
+        const fs=require('fs')
+        const cp=require('child_process');
+
+        const wkspaceFd = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		const logfile=`${wkspaceFd}/log.wmake`;
+
+        // give OpenFOAM path
+        // var of_path = await vscode.window.showInputBox({
+        //     ignoreFocusOut: true, // 鼠标点击其他地方输入框不会消失
+        //     placeHolder: '/opt/openfoam8', // 在输入框内的提示信息
+        //     prompt: '请输入OpenFOAM路径，如/opt/openfoam8或/home/of/OpenFOAM/OpenFOA-8等，默认为/opt/openfoam8' // 在输入框下方的提示信息,
+        // });
+        // of_path  = of_path.length===0 ? '/opt/openfoam8': of_path;
+        var conf=vscode.workspace.getConfiguration('ofextension');
+        console.log('\n=== 当前配置 ===')
+        var OFpath = conf.get('OFpath');
+        // var OFsrc = conf.get('OFsrc') ? conf.get('OFsrc'): `${OFpath}/src`; // 如果没有设置，则默认OFpath下的src
+        var GDBpath = conf.get('GDBpath') ? conf.get('GDBpath'): '/usr/bin/gdb'; 
+        console.log('OpenFOAM-path： '+OFpath);
+        // console.log('OpenFOAM-src： '+OFsrc);
+        console.log('GDB-path： '+GDBpath);
+
+        // premake the solver
+            // 检查是否是求解器目录 
+        const file_Makefiles=`${wkspaceFd}/Make/files`;
+        console.log('\n=== 检查是否为求解器目录 ===')
+        try {
+            fs.accessSync(file_Makefiles,fs.constants.F_OK);
+            console.log(`Yes, ${file_Makefiles} 存在。`)
+        } catch (err) {
+            console.log(`No, ${file_Makefiles} 不存在，请切换到求解器目录。`)
+        }
+        //     // 预编译，得到log.wmake文件用于提取includePath 
+        console.log('\n=== 预编译，获取wmake日志 ===');
+        // Note: 务必修改Make/files文件，使编译结果保存到用户目录
+        // cp.exec(`sed -i "s~FOAM_APPBIN~FOAM_USER_APPBIN~g" ${wkspaceFd}/Make/files`);
+        var cmd=`source ${OFpath}/etc/bashrc ;export WM_COMPILE_OPTION=Debug `; // source ...bashrc 状态码为1，因此不能用&&连接命令
+        var sh=vscode.env.shell
+        console.log(`用于执行命令的shell：${sh}`)
+        cmd+='&& wclean &&  wmake 2>&1 | tee log.wmake'
+        console.log(`执行预编译命令：${cmd}`)
+        // var stdout = cp.exec(cmd,{cwd:wkspaceFd,shell:sh})
+        // console.log('预编译：\n',stdout);
+        cp.exec(cmd,{cwd:wkspaceFd,shell:sh},(err,stdout,stderr)=>{
+            console.log('预编译stdout: \n'+ (stdout?stdout: '无'));
+            console.log('预编译stderr: '+ (stderr?`\n${stderr}`: '无'));
+            // if(err){ console.log('error: '+err); }
+            if(!err){
+                console.log('\n=== 提取wmake日志文件，更新includePath ===')
+                fs.access(`${wkspaceFd}/.vscode/c_cpp_properties.json`,fs.constants.F_OK,(err)=>{
+                    var m = get_includePath(logfile);
+                    var m_uniq=sort_uniq_pp(m);
+                    gen_cpp_conf(err,m_uniq,wkspaceFd);
+                });
+            }
+        });
+        
+        // 从log.wmake中提取includePath
+        // const fs=require('fs');
+        // var data = fs.readFileSync('./log.wmake','utf8');
+        // var m = data.match(/\-I(\S*)/g);
+        // function uniq(arr){
+        //     arr.sort();
+        //     var hash=[arr[0]];
+        //     for (let i = 0; i < arr.length; i++) {
+        //         if (arr[i]!=hash[hash.length-1]) {
+        //             hash.push(arr[i]);
+        //         }
+        //     }
+        //     return hash;
+        // }
+        // function post_process(arr){
+        //     for (let i = 0; i < arr.length; i++) {
+        //         arr[i]=arr[i].replace('-I','');
+        //         arr[i]=arr[i].replace(/$/,'/*');
+        //     }
+        // }
+        // var m_uniq = uniq(m);
+        // post_process(m_uniq);
+        // var fs=require('fs');
+		// var wkspacefd=vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+        // fs.access(`${wkspaceFd}/.vscode/c_cpp_properties.json`,fs.constants.F_OK,(err)=>{
+        //     var m = get_includePath(logfile);
+        //     var m_uniq=sort_uniq_pp(m);
+        //     gen_cpp_conf(err,m_uniq,wkspaceFd);
+        // });
+
+
+        // var gdb_path = await vscode.window.showInputBox({
+        //     ignoreFocusOut: true, // 鼠标点击其他地方输入框不会消失
+        //     placeHolder: '/usr/bin/gdb', // 在输入框内的提示信息
+        //     prompt: '请输入gdb路径。默认使用系统中的gdb，如果自己想使用自己编译的gdb，可在此处指定。' // 在输入框下方的提示信息,
+        // });
+        // gdb_path = gdb_path.length===0? '/usr/bin/gdb': gdb_path;
+        // console.log('gdb路径： '+gdb_path);
+    }));
+};
---
+++ src/vscode.d.ts
@@ -0,0 +1,214 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+declare module 'vscode' {
+
+	/**
+	 * The version of the editor.
+	 */
+	export const version: string;
+
+	/**
+	 * Represents a reference to a command. Provides a title which
+	 * will be used to represent a command in the UI and, optionally,
+	 * an array of arguments which will be passed to the command handler
+	 * function when invoked.
+	 */
+	export interface Command {
+		/**
+		 * Title of the command, like `save`.
+		 */
+		title: string;
+
+		/**
+		 * The identifier of the actual command handler.
+		 * @see [commands.registerCommand](#commands.registerCommand).
+		 */
+		command: string;
+
+		/**
+		 * A tooltip for the command, when represented in the UI.
+		 */
+		tooltip?: string;
+
+		/**
+		 * Arguments that the command handler should be
+		 * invoked with.
+		 */
+		arguments?: any[];
+	}
+
+	/**
+	 * Represents a line of text, such as a line of source code.
+	 *
+	 * TextLine objects are __immutable__. When a [document](#TextDocument) changes,
+	 * previously retrieved lines will not represent the latest state.
+	 */
+	export interface TextLine {
+
+		/**
+		 * The zero-based line number.
+		 */
+		readonly lineNumber: number;
+
+		/**
+		 * The text of this line without the line separator characters.
+		 */
+		readonly text: string;
+
+		/**
+		 * The range this line covers without the line separator characters.
+		 */
+		readonly range: Range;
+
+		/**
+		 * The range this line covers with the line separator characters.
+		 */
+		readonly rangeIncludingLineBreak: Range;
+
+		/**
+		 * The offset of the first character which is not a whitespace character as defined
+		 * by `/\s/`. **Note** that if a line is all whitespace the length of the line is returned.
+		 */
+		readonly firstNonWhitespaceCharacterIndex: number;
+
+		/**
+		 * Whether this line is whitespace only, shorthand
+		 * for [TextLine.firstNonWhitespaceCharacterIndex](#TextLine.firstNonWhitespaceCharacterIndex) === [TextLine.text.length](#TextLine.text).
+		 */
+		readonly isEmptyOrWhitespace: boolean;
+	}
+
+	/**
+	 * Represents a text document, such as a source file. Text documents have
+	 * [lines](#TextLine) and knowledge about an underlying resource like a file.
+	 */
+	export interface TextDocument {
+
+		/**
+		 * The associated uri for this document.
+		 *
+		 * *Note* that most documents use the `file`-scheme, which means they are files on disk. However, **not** all documents are
+		 * saved on disk and therefore the `scheme` must be checked before trying to access the underlying file or siblings on disk.
+		 *
+		 * @see [FileSystemProvider](#FileSystemProvider)
+		 * @see [TextDocumentContentProvider](#TextDocumentContentProvider)
+		 */
+		readonly uri: Uri;
+
+		/**
+		 * The file system path of the associated resource. Shorthand
+		 * notation for [TextDocument.uri.fsPath](#TextDocument.uri). Independent of the uri scheme.
+		 */
+		readonly fileName: string;
+
+		/**
+		 * Is this document representing an untitled file which has never been saved yet. *Note* that
+		 * this does not mean the document will be saved to disk, use [`uri.scheme`](#Uri.scheme)
+		 * to figure out where a document will be [saved](#FileSystemProvider), e.g. `file`, `ftp` etc.
+		 */
+		readonly isUntitled: boolean;
+
+		/**
+		 * The identifier of the language associated with this document.
+		 */
+		readonly languageId: string;
+
+		/**
+		 * The version number of this document (it will strictly increase after each
+		 * change, including undo/redo).
+		 */
+		readonly version: number;
+
+		/**
+		 * `true` if there are unpersisted changes.
+		 */
+		readonly isDirty: boolean;
+
+		/**
+		 * `true` if the document has been closed. A closed document isn't synchronized anymore
+		 * and won't be re-used when the same resource is opened again.
+		 */
+		readonly isClosed: boolean;
+
+		/**
+		 * Save the underlying file.
+		 *
+		 * @return A promise that will resolve to true when the file
+		 * has been saved. If the file was not dirty or the save failed,
+		 * will return false.
+		 */
+		save(): Thenable<boolean>;
+
+		/**
+		 * The [end of line](#EndOfLine) sequence that is predominately
+		 * used in this document.
+		 */
+		readonly eol: EndOfLine;
+
+		/**
+		 * The number of lines in this document.
+		 */
+		readonly lineCount: number;
+
+		/**
+		 * Returns a text line denoted by the line number. Note
+		 * that the returned object is *not* live and changes to the
+		 * document are not reflected.
+		 *
+		 * @param line A line number in [0, lineCount).
+		 * @return A [line](#TextLine).
+		 */
+		lineAt(line: number): TextLine;
+
+		/**
+		 * Returns a text line denoted by the position. Note
+		 * that the returned object is *not* live and changes to the
+		 * document are not reflected.
+		 *
+		 * The position will be [adjusted](#TextDocument.validatePosition).
+		 *
+		 * @see [TextDocument.lineAt](#TextDocument.lineAt)
+		 * @param position A position.
+		 * @return A [line](#TextLine).
+		 */
+		lineAt(position: Position): TextLine;
+
+		/**
+		 * Converts the position to a zero-based offset.
+		 *
+		 * The position will be [adjusted](#TextDocument.validatePosition).
+		 *
+		 * @param position A position.
+		 * @return A valid zero-based offset.
+		 */
+		offsetAt(position: Position): number;
+
+		/**
+		 * Converts a zero-based offset to a position.
+		 *
+		 * @param offset A zero-based offset.
+		 * @return A valid [position](#Position).
+		 */
+		positionAt(offset: number): Position;
+
+		/**
+		 * Get the text of this document. A substring can be retrieved by providing
+		 * a range. The range will be [adjusted](#TextDocument.validateRange).
+		 *
+		 * @param range Include only the text included by the range.
+		 * @return The text inside the provided range or the entire text.
+		 */
+		getText(range?: Range): string;
+
+		/**
+		 * Get a word-range at the given position. By default words are defined by
+		 * common separators, like space, -, _, etc. In addition, per language custom
+		 * [word definitions](#LanguageConfiguration.wordPattern) can be defined. It
+		 * is also possible to provide a custom regular expression.
+		 *
+		 * * *Note 1:* A custom regular expression must not match the empty string and
+		 * if it does, it will be ignored.
+		 *
